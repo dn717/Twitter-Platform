faf31acb4f0b3a1bb6a87e50a7b3fcdf00de081a
dn717 <djiang3@lakeheadu.ca>
2022-10-19 15:51:58 -0400

    create twitter project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NewTweetPageComponent } from './new-tweet-page/new-tweet-page.component';
+import { DetailPageComponent } from './detail-page/detail-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Add Components
+  {path:'',component:HomePageComponent},
+  {path:'home',component:HomePageComponent},
+  {path:'new',component:NewTweetPageComponent},
+  {path:'detail/:id',component:DetailPageComponent},
+  {path:'edit/:id',component:EditPageComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NewTweetPageComponent } from './new-tweet-page/new-tweet-page.component';
+import { DetailPageComponent } from './detail-page/detail-page.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { EditPageComponent } from './edit-page/edit-page.component';
 
 @NgModule({
+  //registring components
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NewTweetPageComponent,
+    DetailPageComponent,
+    EditPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
